4b22c48ebfe25b4a39507002afe21439f1eab39b
Priyanka Sheoran <priyankasheoran151@gmail.com>
2023-07-21 14:39:34 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,70 +1,4 @@
 
-// $("h1").css("color", "red");
-
-// var arr = ["green", "red", "yellow", "blue"];
-// var pattern = [];
-
-// function generateRandomNum(){
-//     x = x * 4;
-//     var x = Math.floor(Math.random()* 4);
-//     // x = x * 4;
-//     // x = Math.floor(x);
-//     // return x;
-// }
-
-
-// var num = 1;
-// // console.log($("h1").html());
-// if($("h1").html() == "Game Over, Press any key to restart" || $("h1").html() == "Press A Key to Start" ){
-//     console.log("true");
-// document.addEventListener("keydown", function () {
-//     // if($("h1").html == "Game Over, Press any key to restart")
-//     // {
-//     //     num = 1;
-//     //     return ;
-//     // }
-//     console.log("Key pressed");
-//     // while( $("h1").html() != "Game Over, Press any key to restart"){
-//         $("h1").html("Level "+ num); 
-//         // console.log("blaaaaaaaaa");
-//         // var x = Math.random();
-//         // x = x * 4;
-//         // x = Math.floor(x);
-        
-//         // alert(x);
-        
-//     var num_random = generateRandomNum();
-//     console.log(num_random); 
-//     // console.log(arr[2]);
-//     // var pattern = [];
-//     pattern.push(num_random);
-//     var i = 0;
-//     while (i < pattern.length && $("h1").html != "Game Over, Press any key to restart") {
-//         console.log("blaaaaaaaaa");
-//     // for(var j = 0; j<4; j++)
-//     // {
-//         $("button").click(function () {
-//         if(this.classList[1] == arr[pattern[i]]){
-//         console.log("yes");
-//         }
-//         // $("h1").html("Level "+ ++num); 
-//         else{
-//         $("h1").html("Game Over, Press any key to restart"); 
-//         }
-//         });
-//         if($("h1").html == "Game Over, Press any key to restart")
-//         break;
-//     // }
-//     i++;
-//     }
-//     num++;
-// // }
-// });
-// }
-
-// // else{
-// //     num = 1;
-// // }
 
 
 var BtnColours = ["green", "red", "yellow", "blue"];
@@ -92,35 +26,6 @@ function nextSequence(){
     // checkSequence();
 }
 
-// function checkSequence(){
-//     if(i == pattern.length)
-//         {
-//             console.log("bdha");
-//             i = 0;
-//             nextSequence();
-//         }
-//     else{
-//     $(".btn").click(function(){
-//         // console.log(this.classList[1]);
-//         // console.log(BtnColours[pattern[i]]);
-//         // console.log(pattern.length);
-        
-//         if(this.classList[1] == BtnColours[pattern[i]])
-//         {
-//             console.log("Enter");
-//             console.log(i);
-//             i++;
-//             $("#"+this.classList[1]).fadeIn(100).fadeOut(100).fadeIn(100);
-//             checkSequence();
-//         }
-//         else{
-//             i = 0;
-//             gameover();
-            
-//         }
-//     });
-// }
-// }
 
 $(".btn").click(function(){
     userclickedpattern.push(this.classList[1]);
